b5c050a81e3dd586a9655b8935bdcf66bd93c143
nathanshiaulam <nlam@princeton.edu>
2014-12-25 23:03:31 -0800

    fixed index.html

---
+++ psibackend/migrations/0027_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('psibackend', '0026_auto_20141224_2122'),
+        ('psibackend', '0024_post_croppedpicture'),
+    ]
+
+    operations = [
+    ]
